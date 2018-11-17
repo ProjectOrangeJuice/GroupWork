1b5c6718423a177f083394a5a1cb9b540c69adbf
Kane <Kane@LAPTOP-911UQCH6>
2018-11-17 17:25:13 +0000

    added loanToUser method alongside needed attributes for borrowing copies

--- src/Resource.java
+++ src/Resource.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
 import javafx.scene.image.Image;
 
 public abstract class Resource {	
@@ -6,11 +10,39 @@ public abstract class Resource {
 	protected int year;
 	protected Image thumbnail;
 	
+	private ArrayList<Copy> copyList;
+	private LinkedList<Copy> freeCopies;
+	private PriorityQueue<Copy> noDueDateCopies;
+	private Queue<User> userRequest;
+	
 	public Resource(int uniqueID, String title, int year, Image thumbnail) {
 		this.uniqueID = uniqueID;
 		this.title = title;
 		this.year = year;
 		this.thumbnail = thumbnail;
+		
+		copyList = new ArrayList<Copy>();
+		freeCopies = new LinkedList<Copy>();
+		noDueDateCopies = new PriorityQueue<Copy>();
+		userRequest = new Queue<User>();
+		
+	}
+	
+	public void loanToUser(User user) 
+	{	
+		if(!freeCopies.isEmpty()) 
+		{
+			Copy copyToBorrow = freeCopies.removeFirst();
+			copyToBorrow.setBorrower(user);
+			user.addBorrowedCopy(copyToBorrow);
+		} 
+		else 
+		{
+			userRequest.enqueue(user);
+			Copy firstCopy = noDueDateCopies.poll();
+			firstCopy.setDueDate();
+		}
+		
 	}
 
 	public int getUniqueID() {
