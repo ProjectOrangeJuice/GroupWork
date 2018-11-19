f1951f632c97e9fe6130370776b74a3eb23a04bf
Oliver Harris <git@harriso.co.uk>
2018-11-19 21:56:19 +0000

    Added examples to the database

--- src/tables.sql
+++ src/tables.sql
@@ -9,17 +9,23 @@ CREATE TABLE IF NOT EXISTS `users` (
 	`profile`	TEXT,
 	`balance`	TEXT
 );
+INSERT INTO `users` VALUES (1,'Bob','Bobby','01234567','Nowhere','','0');
+INSERT INTO `users` VALUES (2,'Tim','Timmy','223242','Elsewhere','','0');
+INSERT INTO `users` VALUES (3,'Staff','Staffy','253325','Here','','0');
+INSERT INTO `users` VALUES (4,'Staffy2','Staffy2y','575686','There','','0');
 DROP TABLE IF EXISTS `system`;
 CREATE TABLE IF NOT EXISTS `system` (
 	`ver`	INTEGER
 );
-INSERT INTO `system` VALUES (1);
+INSERT INTO `system` VALUES (2);
 DROP TABLE IF EXISTS `staff`;
 CREATE TABLE IF NOT EXISTS `staff` (
 	`staffId`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`userId`	INTEGER NOT NULL,
 	`date`	TEXT
 );
+INSERT INTO `staff` VALUES (1,3,'19/11/18');
+INSERT INTO `staff` VALUES (2,4,'1/1/73');
 DROP TABLE IF EXISTS `resource`;
 CREATE TABLE IF NOT EXISTS `resource` (
 	`RId`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -27,6 +33,9 @@ CREATE TABLE IF NOT EXISTS `resource` (
 	`title`	TEXT,
 	`description`	TEXT
 );
+INSERT INTO `resource` VALUES (1,'Book','Bookie',NULL);
+INSERT INTO `resource` VALUES (2,'Laptop','Laptop 1',NULL);
+INSERT INTO `resource` VALUES (3,'Book','Other book',NULL);
 DROP TABLE IF EXISTS `copies`;
 CREATE TABLE IF NOT EXISTS `copies` (
 	`CopyId`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -34,6 +43,16 @@ CREATE TABLE IF NOT EXISTS `copies` (
 	`keeper`	INTEGER,
 	`date`	TEXT
 );
+INSERT INTO `copies` VALUES (1,1,NULL,NULL);
+INSERT INTO `copies` VALUES (2,1,NULL,NULL);
+INSERT INTO `copies` VALUES (3,1,NULL,NULL);
+INSERT INTO `copies` VALUES (4,1,NULL,NULL);
+INSERT INTO `copies` VALUES (5,2,NULL,NULL);
+INSERT INTO `copies` VALUES (6,2,NULL,NULL);
+INSERT INTO `copies` VALUES (7,2,NULL,NULL);
+INSERT INTO `copies` VALUES (8,2,NULL,NULL);
+INSERT INTO `copies` VALUES (9,3,NULL,NULL);
+INSERT INTO `copies` VALUES (10,3,NULL,NULL);
 DROP TABLE IF EXISTS `borrowRecords`;
 CREATE TABLE IF NOT EXISTS `borrowRecords` (
 	`borrowId`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
