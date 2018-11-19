ebce27c9d2370c9551c9698dae8270dcfda0d9c2
Joe Wright <joewright98@live.co.uk>
2018-11-19 12:46:30 +0000

    made copyid final

--- src/Copy.java
+++ src/Copy.java
@@ -3,12 +3,13 @@ public class Copy {
 	
 	private Resource resource;
 	private User borrower;
-	private int copyID;
+	private final int COPY_ID;
 	
 	
-	public Copy(Resource resource) {
+	public Copy(Resource resource, int copyID) {
 		this.resource = resource;
 		this.borrower = null;
+		this.COPY_ID = copyID;
 	}
 
 	public void setBorrower(User borrower) {
@@ -18,5 +19,7 @@ public class Copy {
 	public void setDueDate() {
 		
 	}
+	
+	
 
 }
