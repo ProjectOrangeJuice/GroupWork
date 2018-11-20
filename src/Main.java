9234a68a49143072f11e1f836f7349249f29433b
Charles Day <charlesday99@outlook.com>
2018-11-20 15:49:25 +0000

    Added testing Main back in.

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+import javafx.scene.image.Image;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> languages = new ArrayList<String>();
+		
+		languages.add("English");
+		languages.add("French");
+		languages.add("Romanian");
+		
+		Book Sapiens = new Book(0, "Sapiens", 2017, null, "Noah", "Penguin Books", "educational", "111222333", "English");
+		Book TheMartian = new Book(1, "The Martian", 2015, null, "Andy Weir", "Kane Books", "sci-fi", "333444555", "English");
+		DVD Interstellar = new DVD(2, "Interstellar", 2014, null, "Christopher Nolan", 169, "English", languages);
+		
+		System.out.println(Sapiens.getUniqueID());
+		System.out.println(TheMartian.getUniqueID());
+		System.out.println(Interstellar.getUniqueID());
+
+	}
+}
