ffe94fe09d42c93dca18a4781565635d460970a2
Joe Wright <joewright98@live.co.uk>
2018-11-22 13:36:48 +0000

    sets gets

--- src/Copy.java
+++ src/Copy.java
@@ -1,3 +1,4 @@
+import java.sql.Date;
 
 public class Copy {
 	
@@ -5,24 +6,68 @@ public class Copy {
 	private int borrower;
 	private final int COPY_ID;
 	
-	
+	/**
+	 * Class Constructor
+	 * @param resource
+	 * @param copyID
+	 * @param borrower
+	 */
 	public Copy(Resource resource, int copyID, int borrower) {
 		this.resource = resource;
 		this.borrower = borrower;
 		this.COPY_ID = copyID;
 	}
-
+    
+	/**
+	 * 
+	 * @param borrower
+	 */
 	public void setBorrower(int borrower) {
 		this.borrower = borrower;
 	}
 	
+	/**
+	 * 
+	 */
 	public void setDueDate() {
 		
+	}
+	
+	/**
+	 * Method that gets the duedate variable
+	 * @return duedate
+	 */
+	public Date getDueDate(){
+		return duedate;
+	}
+	
+	/**
+	 * Method that gets the duration variable
+	 * @return duration
+	 */
+	public Date getDuration(){
+		return duration;
+	}
+	
+	/**
+	 * Method that gets the resource variable
+	 * @return resource
+	 */
+	public Resource getResource(){
+		return resource;
+	}
+	
+	/**
+	 * Method that gets the COPY_ID final variable
+	 * @return COPY_ID
+	 */
+	public int getCOPY_ID(){
+		return COPY_ID;
 	}
 	
 	
 	
 	
-	
+
 
 }
