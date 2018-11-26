75c6fb7fd50ed772f09beccea69edd6b53abc301
Oliver <git@harriso.co.uk>
2018-11-26 21:10:03 +0000

    Connected database to transactions

--- src/Transactions.java
+++ src/Transactions.java
@@ -1,16 +1,21 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Transactions {
 
 	private int username;
 	private ArrayList<Payment> payments;
-	private ArrayList<Fine> fines;
+
 	
 	
-	public Transactions(int username, ArrayList<Payment> payments, ArrayList<Fine> fines) {
+	public Transactions(int username, ArrayList<Payment> payments) {
 		this.username = username;
 		this.payments = payments;
-		this.fines = fines;
+
 	}
 	
 	public int getUsername() {
@@ -27,15 +32,24 @@ public class Transactions {
 	
 	public void addPayment(Payment payment) {
 		payments.add(payment);
+		String stamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+		try {
+			Connection conn = DBHelper.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("INSERT INTO transactions (userId,paid,dateTime) VALUES (?,?,?)"); 
+	            pstmt.setInt(1, username);
+	            pstmt.setFloat(2, payment.getAmount());
+	            pstmt.setString(3, stamp);
+	            pstmt.executeUpdate();
+
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	public ArrayList<Fine> getFines() {
-		return fines;
-	}
 	
-	public void addFines(Fine fine) {
-		fines.add(fine);
-	}
 	
 	
 	
