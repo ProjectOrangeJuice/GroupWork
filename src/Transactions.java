42a977f47e9911c1a465c15258924d5d0f513551
oharris <oharris@Desktop>
2018-11-26 20:55:17 +0000

    Added setters and getters

--- src/Transactions.java
+++ src/Transactions.java
@@ -1,6 +1,41 @@
+import java.util.ArrayList;
 
 public class Transactions {
 
+	private int username;
+	private ArrayList<Payment> payments;
+	private ArrayList<Fine> fines;
+	
+	
+	public Transactions(int username, ArrayList<Payment> payments, ArrayList<Fine> fines) {
+		this.username = username;
+		this.payments = payments;
+		this.fines = fines;
+	}
+	
+	public int getUsername() {
+		return username;
+	}
+	
+	public void setUsername(int username) {
+		this.username = username;
+	}
+	
+	public ArrayList<Payment> getPayments() {
+		return payments;
+	}
+	
+	public void addPayment(Payment payment) {
+		payments.add(payment);
+	}
+	
+	public ArrayList<Fine> getFines() {
+		return fines;
+	}
+	
+	public void addFines(Fine fine) {
+		fines.add(fine);
+	}
 	
 	
 	
