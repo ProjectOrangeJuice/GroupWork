28843a099612c661771d2b1d9428135d8432329e
Oliver <git@harriso.co.uk>
2018-12-01 17:29:38 +0000

    Transaction controller created

---
+++ src/application/TransactionsController.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+
+public class TransactionsController {
+	
+	
+	@FXML  
+    void transactionSearch(KeyEvent event) {
+        
+        }
+    
+
+
+	
+	@FXML
+	 public void initialize() {
+		int user = 1;
+		
+		
+	 }    
+}
