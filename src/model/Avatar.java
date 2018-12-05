896432c9534e983d05ad4251e8ff0ef4ef253674
Finz95 <33931822+Finz95@users.noreply.github.com>
2018-12-05 15:58:49 +0000

    PI

--- src/model/Avatar.java
+++ src/model/Avatar.java
@@ -5,11 +5,11 @@ import javafx.scene.image.ImageView;
  * @author James Finlayson
  */
 public abstract class Avatar {
-    //Number of dimensions a profile image has
+    //Number of dimensions an avatar has
     public static final int NO_OF_DIMENSIONS = 2;
     public static final int X_INDEX = 0; //x index
     public static final int Y_INDEX = 1; //y index
-    private double size; //size (of profile image)
+    private double size; //size of the avatar
     private double[] position; //centre x,y coordinates 
 
     /**
@@ -27,16 +27,16 @@ public abstract class Avatar {
     }
 
     /**
-     * @return The size of the profile image.
+     * @return The size of the avatar.
      */
     public double getSize() {
         return size;
     }
 
     /**
-     * Gets x-axis centre position
+     * Gets x-axis centre position.
      *
-     * @return The x position of the centre of the profile image.
+     * @return The x position of the centre of the avatar.
      */
     public double getXPosition() {
         return position[X_INDEX];
@@ -45,7 +45,7 @@ public abstract class Avatar {
     /**
  	 * Gets y-axis centre position
  	 * 
-     * @return The y position of the centre of the profile image.
+     * @return The y position of the centre of the avatar.
      */
     public double getYPosition() {
         return position[Y_INDEX];
@@ -54,7 +54,7 @@ public abstract class Avatar {
     /**
      * Sets size variable
      *
-     * @param size The size of the profile image.
+     * @param size The size of the avatar.
      */
     public void setSize(double size) {
         this.size = size;
@@ -63,7 +63,7 @@ public abstract class Avatar {
     /**
      * Sets x-axis centre position
      *
-     * @param x The x position of the centre of the profile image.
+     * @param x The x position at the centre of the avatar.
      */
     public void setXPosition(double x) {
         this.position[X_INDEX] = x;
@@ -72,14 +72,14 @@ public abstract class Avatar {
     /**
      * Sets y-axis centre position     
      * 
-     * @param y The y position of the centre of the profile image.
+     * @param y The y position at the centre of the avatar.
      */
     public void setYPosition(double y) {
         this.position[Y_INDEX] = y;
     }
 
     /**
-     * Converts a profile image to a String.
+     * Converts an avatar to a String.
      */
     public String toString() {
         String result = "";
@@ -90,8 +90,8 @@ public abstract class Avatar {
     }
 
     /**
-     * Displays the profile image on an ImageView.
-     * @param imageView The ImageView that the profile image is being displayed on.
+     * Displays the avatar on an ImageView.
+     * @param imageView The ImageView that the avatar is being displayed on.
      */
     public abstract void displayAvatar(ImageView imageView);
 }
--- src/model/Circle.java
+++ src/model/Circle.java
@@ -1,5 +1,53 @@
 package model;
 
-public class Circle {
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
+/**
+ * @author James Finlayson
+ * @version 1.0
+ */
+public class Circle extends AvatarComponent {
+    private double radius; //The radius of the circle, from the starting point.
+
+    /**
+     * Creates a circle.
+     *
+     * @param xStartPosition The x start position.
+     * @param yStartPosition The y start position.
+     * @param colour         The colour of the circle.
+     * @param radius         The radius of the circle.
+     */
+    public Circle(double xStartPosition, double yStartPosition, Color colour,
+                  double radius) {
+        super(xStartPosition, yStartPosition, colour);
+        this.radius = radius;
+    }
+
+    /**
+     * Gets the radius of the circle.
+     *
+     * @return The radius of the circle.
+     */
+    public double getRadius() {
+        return radius;
+    }
+
+    /**
+     * Resets the radius of the circle.
+     *
+     * @param radius The radius of the circle.
+     */
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void displayComponent(Canvas canvas) {
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+        graphicsContext.setFill(getColour());
+        graphicsContext.fillOval(getXStartPosition(), getYStartPosition(),
+                getRadius(), getRadius());
+    }
 }
