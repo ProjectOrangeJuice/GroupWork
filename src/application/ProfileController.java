f7965bd8e6823ea865bd43fec42a98ee555428c9
Oliver <git@harriso.co.uk>
2018-12-08 16:50:54 +0000

    Changed £

--- src/application/ProfileController.java
+++ src/application/ProfileController.java
@@ -140,6 +140,9 @@ public class ProfileController {
 	@FXML
 	private TableView staffCopiesExplorerTable;
 	
+	@FXML
+	private VBox leftVbox;
+	
 	//may remove fixed size resource images
 	//when dealing with window resizing.
 	private final int RES_IMG_WIDTH = 150;
@@ -213,7 +216,7 @@ public class ProfileController {
 			phoneLabel.setText(phoneLabel.getText() + " " + phoneNumber);
 			
 			Double userBalance = ((User) currentUser).getAccountBalance();
-			accountBalance.setText("�" + Double.toString(userBalance));
+			accountBalance.setText("£" + Double.toString(userBalance));
 		}else {
 			//get all information in about user from ScreenManager class.
 			Librarian staff = (Librarian) currentUser;
