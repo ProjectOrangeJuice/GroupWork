8d9da22b499b775837cb854fbad3b47311ad2214
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2018-12-09 18:00:45 +0000

    code conventions are fun

--- src/model/ParticleTrace.java
+++ src/model/ParticleTrace.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.LinkedList;
+
 import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
 
@@ -9,28 +10,30 @@ import javafx.scene.paint.Color;
  * @version 1.0
  */
 public class ParticleTrace extends AvatarComponent {
-	private LinkedList<Circle> particleTrace;
-	
-	/**
-	 * Creates a blank particle trace.
-	 * @param xStartPosition The x start coordinate.
-	 * @param yStartPosition The y start coordinate.
-	 * @param colour The colour of the component.
-	 */
-	public ParticleTrace(double xStartPosition, double yStartPosition, Color colour) {
-		super(xStartPosition, yStartPosition, colour);
-		particleTrace = new LinkedList<Circle>();
-	}
+    private LinkedList<Circle> particleTrace;
 
-	/**
-	 * Extends the particle trace.
-	 * @param c The circle that extends the particle trace.
-	 */
-	public void addToTrace(Circle c) {
-		particleTrace.add(c);
-	}
-	
-	@Override
-	public void displayComponent(Canvas canvas) {
-	}
+    /**
+     * Creates a blank particle trace.
+     * 
+     * @param xStartPosition The x start coordinate.
+     * @param yStartPosition The y start coordinate.
+     * @param colour The colour of the component.
+     */
+    public ParticleTrace(double xStartPosition, double yStartPosition, Color colour) {
+        super(xStartPosition, yStartPosition, colour);
+        particleTrace = new LinkedList<Circle>();
+    }
+
+    /**
+     * Extends the particle trace.
+     * 
+     * @param c The circle that extends the particle trace.
+     */
+    public void addToTrace(Circle c) {
+        particleTrace.add(c);
+    }
+
+    @Override
+    public void displayComponent(Canvas canvas) {
+    }
 }
