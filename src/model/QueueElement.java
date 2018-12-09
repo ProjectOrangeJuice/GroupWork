d86ee8d9aecda02ba3f42c877eee17cb73283a2b
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2018-12-09 22:35:26 +0000

    Completed QueueElement javadoc

--- src/model/QueueElement.java
+++ src/model/QueueElement.java
@@ -1,43 +1,53 @@
 package model;
 
 /**
- * You must use this class when constructing your Queue. This class represents a
- * link of the linked list. Your queue will be composed of these links.
+ * Represents an object that encapsulates an element it store and a reference to 
+ * the next Queue Element.
  * 
- * @author archam
+ * @param <E> Type of element to be stored.
+ * @author Alexandru Dascalu
  */
 public class QueueElement<E> {
     private E element; // the element contained in this linked list
     private QueueElement<E> next; // the next element of the linked list
 
-    public QueueElement(E e, QueueElement<E> n) {
-        this.element = e;
-        this.next = n;
+    /**
+     * Makes a new queue element with the given element and next queue element.
+     * @param element The element to be stored.
+     * @param next The next QueueElement in the queue.
+     */
+    public QueueElement(E element, QueueElement<E> next) {
+        this.element = element;
+        this.next = next;
     }
 
     /**
-     * Method to set the element
+     * Method to set the element.
+     * @param element The new value of the element.
      */
     public void setElement(E element) {
         this.element = element;
     }
 
     /**
-     * Method to set the next linked list element
+     * Method to set the next linked list element.
+     * @param next The new next element.
      */
-    public void setNext(QueueElement<E> e) {
-        this.next = e;
+    public void setNext(QueueElement<E> next) {
+        this.next = next;
     }
 
     /**
      * Method to get the element.
+     * @return The element being stored.
      */
     public E getElement() {
         return element;
     }
 
     /**
-     * Method to get the next linked list element
+     * Method to get the next linked list element.
+     * @return The next element in the queue.
      */
     public QueueElement<E> getNext() {
         return next;
