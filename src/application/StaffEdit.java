9f90827f8923380fbed3a757b456c0fd93f12169
leezhinghang <leezhinghang@192.168.0.18>
2018-12-09 23:03:46 +0000

    commit to merge with master

--- src/application/StaffEdit.java
+++ src/application/StaffEdit.java
@@ -2,6 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -21,6 +22,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import model.DBHelper;
 import model.Librarian;
 import model.Person;
 
@@ -40,6 +42,21 @@ public class StaffEdit {
 
     @FXML
     private TextField firstnameText;//firstname text box
+    
+    @FXML
+    private Label firstNameError;
+    
+    @FXML
+    private Label lastNameError;
+    
+    @FXML
+    private Label phoneNumberError;
+    
+    @FXML
+    private Label addressError;
+    
+    @FXML
+    private Label postcodeError;
 
     @FXML
     private TextField lastnameText;//lastname text box
@@ -51,10 +68,10 @@ public class StaffEdit {
     private TextField postcodeText;//postcode text box
 
     @FXML
-    private TextField staffIDText;//staffid text box
+    private Label staffIDLabel;//staffid text box
 
     @FXML
-    private TextField employmentDateText;//employmentdate text box
+    private Label employmentDateLabel;//employmentdate text box
 
     @FXML
     private TextField phoneNumberText;//phone number text box
@@ -100,7 +117,9 @@ public class StaffEdit {
 	 */
     @FXML
     void cancelEditProfile(MouseEvent event) {
-    	changeScene(event, "/fxml/profileScene.fxml");
+    	Stage stage = (Stage) cancelButton.getScene().getWindow();
+    	stage.close();
+    	//changeScene(event, "/fxml/profileScene.fxml");
     }
 
     /**
@@ -114,39 +133,72 @@ public class StaffEdit {
     	String phoneNumber = phoneNumberText.getText();
     	String address = addressText.getText();
     	String postcode = postcodeText.getText();
-    	String staffID = staffIDText.getText();
-    	String employmentDate = employmentDateText.getText();
+    	String staffID = staffIDLabel.getText();
+    	String employmentDate = employmentDateLabel.getText();
     	String username = usernameLabel.getText();
     	
-    	try {
-    		//SQL that saves the profile information into the table
-    		Connection conn = model.DBHelper.getConnection();
-    		PreparedStatement pstmt = conn.prepareStatement("UPDATE users SET firstName = ?, lastName = ?, telephone = ?, address = ?, postcode = ? WHERE username = ?");
-    		pstmt.setString(1, firstName);
-   			pstmt.setString(2, lastName);
-   			pstmt.setString(3, phoneNumber);
-   			pstmt.setString(4, address);
-   			pstmt.setString(5, postcode);
-   			pstmt.setString(6, username);
-    		pstmt.executeUpdate();
-    		//conn.commit();
-    		
-    		PreparedStatement pstmt1 = conn.prepareStatement("UPDATE staff SET staffID = ?, employmentDate = ?  WHERE staff.username = ?");
-    		pstmt1.setInt(1, Integer.parseInt(staffID));
-   			pstmt1.setString(2, employmentDate);
-   			pstmt.setString(3,  username);
-    			
-   			pstmt1.executeUpdate();
-   			//conn.commit();
-   			
-   			conn.close();
-   			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	alertDone("Details have been saved");
-    	changeScene(event, "/fxml/LoginScene.fxml");
+    	if (validateFirstName()) {
+            firstNameError.setVisible(false);
+        } else {
+            firstNameError.setVisible(true);
+        }
+    	
+    	if (validateLastName()) {
+            lastNameError.setVisible(false);
+        } else {
+            lastNameError.setVisible(true);
+        }
+    	
+    	if (validatePhoneNumber()) {
+            phoneNumberError.setVisible(false);
+        } else {
+            phoneNumberError.setVisible(true);
+        }
+    	
+    	if (validateAddress()) {
+            addressError.setVisible(false);
+        } else {
+            addressError.setVisible(true);
+        }
+    	
+    	if (validatePostCode()) {
+            postcodeError.setVisible(false);
+        } else {
+            postcodeError.setVisible(true);
+        }
+    	
+    	if (validateFirstName() && validateLastName() && validatePhoneNumber() 
+    			&& validateAddress() && validatePostCode()) {
+    		try {
+        		//SQL that saves the profile information into the table
+        		Connection conn = model.DBHelper.getConnection();
+        		PreparedStatement pstmt = conn.prepareStatement("UPDATE users SET firstName = ?, lastName = ?, telephone = ?, address = ?, postcode = ? WHERE username = ?");
+        		pstmt.setString(1, firstName);
+       			pstmt.setString(2, lastName);
+       			pstmt.setString(3, phoneNumber);
+       			pstmt.setString(4, address);
+       			pstmt.setString(5, postcode);
+       			pstmt.setString(6, username);
+        		pstmt.executeUpdate();
+        		//conn.commit();
+        		
+        		PreparedStatement pstmt1 = conn.prepareStatement("UPDATE staff SET staffID = ?, employmentDate = ?  WHERE staff.username = ?");
+        		pstmt1.setInt(1, Integer.parseInt(staffID));
+       			pstmt1.setString(2, employmentDate);
+       			pstmt.setString(3,  username);
+        			
+       			pstmt1.executeUpdate();
+       			//conn.commit();
+       			
+       			conn.close();
+       			
+    		} catch (SQLException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+        	alertDone("Details have been saved");
+        	changeScene(event, "/fxml/LoginScene.fxml");
+    	}
     }
     
     /**
@@ -173,8 +225,8 @@ public class StaffEdit {
 	    	phoneNumberText.setText(phoneNumber);
 	    	addressText.setText(address);
 	    	postcodeText.setText(postcode);
-	    	staffIDText.setText(Integer.toString(staffID));
-	    	employmentDateText.setText(employmentDate);
+	    	staffIDLabel.setText(Integer.toString(staffID));
+	    	employmentDateLabel.setText(employmentDate);
     	}
     }
     
@@ -201,5 +253,47 @@ public class StaffEdit {
 
 		alert.showAndWait();
 	}
+	
+	private boolean validateAddress() {
+		String address = addressText.getText();
+		return !(address.length() > 1000 || address.length() <= 0);
+	}
+	
+	private boolean validatePhoneNumber() {
+		String phoneNumber = phoneNumberText.getText();
+		return !(phoneNumber.length() > 11 || phoneNumber.length() < 11 || !isNumeric(phoneNumber));
+	}
+	
+	private boolean validateLastName() {
+		 String lastName = lastnameText.getText();
+		 
+		return !(lastName.length() > 15 || lastName.length() <= 0);
+	}
 
+	private boolean validateFirstName() {
+		String firstName = firstnameText.getText();
+		 
+		return !(firstName.length() > 15 || firstName.length() <= 0);
+	}
+
+	
+	/**
+	 * Checks if a string consists of numbers.
+	 *
+	 * @param str
+	 *            string to check.
+	 * @return true if String = numbers.
+	 */
+	private boolean isNumeric(String str) {
+		return str.matches(".*\\d+.*");
+	}
+	
+	private boolean validatePostCode() {
+		String postCode = postcodeText.getText();
+		if (postCode.contains(" ")) {
+			
+			return !(postCode.length() > 8 || postCode.length() <= 6);
+		}
+		return !(postCode.length() > 7 || postCode.length() <= 5);
+	}
 }
