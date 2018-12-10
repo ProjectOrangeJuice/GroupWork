bdc3a91e61b97dc39625e7c89901aa5e66270e30
leezhinghang <leezhinghang@192.168.0.18>
2018-12-10 03:31:38 +0000

    Added class description and author to LoginController, ProfileController, RegisterController and StaffEdit

--- src/application/RegisterController.java
+++ src/application/RegisterController.java
@@ -34,7 +34,8 @@ import javafx.stage.Stage;
 import model.DBHelper;
 
 /**
- *
+ * This class allows user to register into the library system. They can toggle choose whether they would like to
+ * register as a new user or a new librarian.
  * @author James Finlayson
  *
  */
