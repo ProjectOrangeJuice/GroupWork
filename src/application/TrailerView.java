61c2c85011e4d42ea4381a7ea87a2c77b26ab5a2
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2019-03-02 15:25:09 +0000

    made the code not crash and and return valid json string

--- src/application/TrailerView.java
+++ src/application/TrailerView.java
@@ -2,15 +2,17 @@ package application;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 import javafx.scene.media.MediaView;
 
-public class TrailerView extends MediaView
+public class TrailerView
 {
-    public void test()
+    public static void main(String args[]) throws UnirestException
     {
         HttpResponse<String> response = Unirest.get("https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query=Thor&language=en-US&api_key=fde767385d9021cca4adc2853f21a53f")
-                .body("{}")
                 .asString();
+        
+        System.out.println(response.getBody());
     }
 }
