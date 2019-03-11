66b3410eb5e787a1c698bf300c6946a87e1fe601
Kane <Kane@DESKTOP-C5UAJTD>
2019-03-11 20:02:34 +0000

    Made start on Event Creation Controller.

---
+++ src/application/EventCreationController.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class EventCreationController {
+
+	@FXML
+	private TextField eventNameField;
+	
+	@FXML
+	private TextField eventDetailsField;
+	
+	@FXML
+	private DatePicker datePickerField;
+	
+	@FXML
+	private TextField maxAttendingField;
+
+	public void createEvent() {
+		
+		
+		
+	}
+
+}
