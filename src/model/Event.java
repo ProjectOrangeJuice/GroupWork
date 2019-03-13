ff817b7860121ea013be576c5c3422fb2625bd0b
Kane <Kane@DESKTOP-C5UAJTD>
2019-03-13 21:29:25 +0000

    Made more slight changes

--- src/model/Event.java
+++ src/model/Event.java
@@ -101,7 +101,7 @@ public class Event {
 		
 		Connection connectionToDB = DBHelper.getConnection();
         Statement stmt = connectionToDB.createStatement();
-        stmt.executeQuery("UPDATE events SET title = " + event.title + ", details = " +
+        stmt.execute("UPDATE events SET title = " + event.title + ", details = " +
         event.details + ", date = " + event.date + ", maxAllowed = " + event.maxAttending + " WHERE eID = " + event.ID);
 
 	}
