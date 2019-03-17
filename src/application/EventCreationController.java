a6a45f7c1bbb4f754bcae76d797699d1cde6bff1
kane <kane@kane-Lenovo-ideapad-510S-14ISK>
2019-03-17 22:48:03 +0000

    Added more comments.

--- src/application/EventCreationController.java
+++ src/application/EventCreationController.java
@@ -74,34 +74,41 @@ public class EventCreationController {
             Connection connectionToDB = DBHelper.getConnection();
             PreparedStatement sqlStatement = connectionToDB.prepareStatement("INSERT INTO events VALUES (?,?,?,?,?)");
             
+            //get LocalDateTime object from datePickerField and timePickerField.
             LocalDateTime dateTime = LocalDateTime.of(datePickerField.getValue(), LocalTime.parse(timePickerField.getText()));
             String eventDate = dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
             
+            //get event name, details and max attendant values from fields.
             String eventName = eventNameField.getText();
             String eventDetails = eventDetailsField.getText();
             int maxAttending = Integer.parseInt(maxAttendingField.getText());
             
-            Event.setTotalEventNo(Event.getTotalEventNo() + 1);
-            
+            //set values in prepared statement.
             sqlStatement.setInt(1, Event.getTotalEventNo());
             sqlStatement.setString(2, eventName);
             sqlStatement.setString(3, eventDetails);
             sqlStatement.setString(4, eventDate);
             sqlStatement.setInt(5, maxAttending);
             
+            //execute SQL insert and close connection.
             sqlStatement.execute();
             connectionToDB.close();
             
+            //increment total event number.
+            Event.setTotalEventNo(Event.getTotalEventNo() + 1);
+            //add event to allEvents.
             Event.addEvent(eventName, eventDetails, eventDate, maxAttending);
-            
+            //close the stage.
             Stage stage = (Stage) createEventButton.getScene().getWindow();
     	    stage.close();
     
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (DateTimeParseException e) {
+        	//if invalid date/time, show error message.
         	Alert alert = new Alert(AlertType.WARNING, "Please enter a valid event time.", ButtonType.OK);
         	alert.show();
+        	//if invalid max attendant number, show error message.
         } catch (NumberFormatException e) {
         	Alert alert = new Alert(AlertType.WARNING, "Please enter a valid number of spaces.", ButtonType.OK);
         	alert.show();
