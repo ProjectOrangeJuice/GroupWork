058bff9dfc0c14f6ff51ab261394e2af94df6e16
the-juggernaut <git@the-juggernaut.net>
2019-03-17 23:16:09 +0000

    Part1 finish stat controller

--- src/application/UserStatisticsController.java
+++ src/application/UserStatisticsController.java
@@ -27,20 +27,7 @@ import model.Person;
 
 public class UserStatisticsController {
 
-	// date range for charts
-	private Date desiredDate1 = null;
-	private Date desiredDate2 = null;
-
 	
-	private final String daysOfTheWeek[] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
-			"Sunday" };
-
-	// the format of a date used to set the desiredDate 1 and 2
-	private final String FORMAT_YMDHMS = new String("yyyy-MM-dd HH:mm:ss");
-
-	int borrowed;
-
-	DBHelper helper = new DBHelper();
 	Person person = ScreenManager.getCurrentUser();
 	String username = person.getUsername();
 
@@ -51,8 +38,8 @@ public class UserStatisticsController {
 	public void initialize() throws SQLException {
 		
 		this.initializeMonthlyStatsGraph();
-	//	this.initializeWeeklyStatsGraph();
-		//this.initializeDailyStatsGraph();
+		this.initializeWeeklyStatsGraph();
+		this.initializeDailyStatsGraph();
 
 	
 
@@ -112,8 +99,117 @@ public class UserStatisticsController {
 		int monthStat = model.Statistics.totalBorrow(username,
 				date2,date1);
 		System.out.println("This month : "+monthStat);
+	}
 		
 		
+		public void initializeWeeklyStatsGraph() {
+			String month1;
+			String month2;
+			String day1;
+			String day2;
+			Date dt = new Date();
+			Calendar c = Calendar.getInstance(); 
+			c.setTime(dt); 
+			c.add(Calendar.DATE, 1);
+			dt = c.getTime();
+			int month1T = c.get(Calendar.MONTH) + 1; // beware of month indexing from zero
+			int year1  = c.get(Calendar.YEAR);
+			int day1T = c.get(Calendar.DAY_OF_MONTH)-1;
+			if(day1T<10) {
+				day1 = "0"+day1T;
+			}else {
+				day1 = String.valueOf(day1T);
+			}
+			if(month1T<10) {
+				month1 = "0"+month1T;
+			}else {
+				month1 = String.valueOf(month1T);
+			}
+			c.add(Calendar.DAY_OF_MONTH, -7);
+			int month2T = c.get(Calendar.MONTH) + 1; // beware of month indexing from zero
+			int year2  = c.get(Calendar.YEAR);
+			int day2T = c.get(Calendar.DAY_OF_MONTH)-1;
+			
+			if(day1T<10) {
+				day2 = "0"+day2T;
+			}else {
+				day2 = String.valueOf(day2T);
+			}
+			
+			if(month1T<10) {
+				month2 = "0"+month2T;
+			}else {
+				month2 = String.valueOf(month2T);
+			}
+			
+			String date1 = year1+"-"+month1+"-"+day1+" 23:59:59";
+			String date2 = year2+"-"+month2+"-"+day2+" 23:59:59";
+			
+			
+			
+			
+			
+			System.out.println(date1.toString());
+			System.out.println(date1.toString());
+			int monthStat = model.Statistics.totalBorrow(username,
+					date2,date1);
+			System.out.println("This Week : "+monthStat);
+		}
+		
+		
+		public void initializeDailyStatsGraph() {
+			String month1;
+			String month2;
+			String day1;
+			String day2;
+			Date dt = new Date();
+			Calendar c = Calendar.getInstance(); 
+			c.setTime(dt); 
+			c.add(Calendar.DATE, 1);
+			dt = c.getTime();
+			int month1T = c.get(Calendar.MONTH) + 1; // beware of month indexing from zero
+			int year1  = c.get(Calendar.YEAR);
+			int day1T = c.get(Calendar.DAY_OF_MONTH)-1;
+			if(day1T<10) {
+				day1 = "0"+day1T;
+			}else {
+				day1 = String.valueOf(day1T);
+			}
+			if(month1T<10) {
+				month1 = "0"+month1T;
+			}else {
+				month1 = String.valueOf(month1T);
+			}
+			c.add(Calendar.DAY_OF_MONTH, -1);
+			int month2T = c.get(Calendar.MONTH) + 1; // beware of month indexing from zero
+			int year2  = c.get(Calendar.YEAR);
+			int day2T = c.get(Calendar.DAY_OF_MONTH)-1;
+			
+			if(day1T<10) {
+				day2 = "0"+day2T;
+			}else {
+				day2 = String.valueOf(day2T);
+			}
+			
+			if(month1T<10) {
+				month2 = "0"+month2T;
+			}else {
+				month2 = String.valueOf(month2T);
+			}
+			
+			String date1 = year1+"-"+month1+"-"+day1+" 23:59:59";
+			String date2 = year2+"-"+month2+"-"+day2+" 23:59:59";
+			
+			
+			
+			
+			
+			System.out.println(date1.toString());
+			System.out.println(date1.toString());
+			int monthStat = model.Statistics.totalBorrow(username,
+					date2,date1);
+			System.out.println("24hours : "+monthStat);
+		}
 //}
 //
 //	public void initializeWeeklyStatsGraph() throws SQLException {
@@ -217,6 +313,6 @@ public class UserStatisticsController {
 //			series.getData().add(new XYChart.Data<Number, Number>(0, borrowedThisHour));
 //		}
 	}
-}
+
 
 
